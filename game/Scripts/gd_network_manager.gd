f3d64ebe9ab5b78a53a5561fa9e2c0061e5a6a12
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-03-23 18:17:50 -0400

    Update git ignore

--- game/Scripts/gd_network_manager.gd
+++ game/Scripts/gd_network_manager.gd
@@ -1,5 +1,1 @@
 extends GDNetworkManager
-
-func _on_packet_received(ip: String, port: int, data: PackedByteArray) -> void:
-	print("Received packet from : %s:%d / Message : %s" % [ip, port, data.get_string_from_ascii()])
-	
